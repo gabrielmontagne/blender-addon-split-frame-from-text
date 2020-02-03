2b8bb410e44c243b271cfcbc9e6ab751bd6d2a41
gabriel montagné láscaris-comneno <gabriel@tibas.london>
2020-02-03 09:23:47 +0000

    stub clone and move

---
+++ clone_frame_from_text.py
@@ -0,0 +1,36 @@
+import bpy
+from random import randint
+
+class NODES_OP_clone_frame(bpy.types.Operator):
+    """Clone frame from text"""
+    bl_idname = "node.clone_frame_from_text"
+    bl_label = "Clone frame from text"
+
+    @classmethod
+    def poll(cls, context):
+        return context.area.type == 'NODE_EDITOR' and context.active_node and context.active_node.type == 'FRAME'
+
+    def execute(self, context):
+        print('execute!')
+        print(context.area.type)
+        print(context.area)
+        print(context.active_node)
+        print(dir(context.active_node))
+        bpy.ops.node.duplicate()
+        bpy.ops.node.translate_attach(TRANSFORM_OT_translate={'value': (100, -100, 0)})
+        context.active_node.label = str(randint(0, 20))
+        print('after', a)
+        return {'FINISHED'}
+
+def register():
+    print('reg')
+    bpy.utils.register_class(NODES_OP_clone_frame)
+
+def unregister():
+    print('unreg')
+    bpy.utils.unregister_class(NODES_OP_clone_frame)
+
+if __name__ == "__main__":
+    register()
+
+
